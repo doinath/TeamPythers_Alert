b8856424e7ef18becde99c00f3559d86acfe50bc
Julian <julianramilandales@gmail.com>
2025-10-18 16:41:00 +0800

    emergency part

--- alertmanagement/emergency/views.py
+++ alertmanagement/emergency/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class HomepageView(View):
+    template_name = "EmergencyEvent/index.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+def login_view(request):
+    return render(request, "EmergencyEvent/login.html")
+
+
+# --- EventAssignment Views ---
+class CreateAssignmentView(View):
+    template_name = "EventAssignment/create_assignment.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class ReceiveAssignmentView(View):
+    template_name = "EventAssignment/receive_assignment.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+# --- EmergencyEvent Views ---
+class ReceiveMessageView(View):
+    template_name = "EmergencyEvent/receive_message.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class ReceiveCallView(View):
+    template_name = "EmergencyEvent/receive_call.html"
+
+    def get(self, request):
+        return render(request, self.template_name)

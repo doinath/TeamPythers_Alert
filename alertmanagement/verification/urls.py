eec3b5c77fe14b8af9b44c6c8a3dddeda1bdd298
doinath <nathanaeljedd.delcastillo@cit.edu>
2025-11-22 00:01:49 +0800

    minor changes in verification/urls.py

--- alertmanagement/verification/urls.py
+++ alertmanagement/verification/urls.py
@@ -7,7 +7,7 @@ app_name = 'verification'
 # path('messages/', MessageListView.as_view(), name='message_list'),
 
 urlpatterns = [
-    path("list", RoleVerification.as_view(), name="verification"),
-    path("goverment", GovernmentDocument.as_view(), name="document"),
-    path("submitted", SubmittedDocument.as_view(), name="submitted"),
+    path("list/", RoleVerification.as_view(), name="verification"),
+    path("goverment/", GovernmentDocument.as_view(), name="document"),
+    path("submitted/", SubmittedDocument.as_view(), name="submitted"),
 ]

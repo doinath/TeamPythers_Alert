69d10f03ee8b4c9fdc5496d0819a911eddbf449d
doinath <nathanaeljedd.delcastillo@cit.edu>
2025-11-21 23:31:45 +0800

    added paths

--- alertmanagement/verification/urls.py
+++ alertmanagement/verification/urls.py
@@ -7,7 +7,7 @@ app_name = 'verification'
 # path('messages/', MessageListView.as_view(), name='message_list'),
 
 urlpatterns = [
-    path("", RoleVerification.as_view(), name="verification"),
-    path("", GovernmentDocument.as_view(), name="document"),
-    path("", SubmittedDocument.as_view(), name="submitted"),
+    path("list", RoleVerification.as_view(), name="verification"),
+    path("goverment", GovernmentDocument.as_view(), name="document"),
+    path("submitted", SubmittedDocument.as_view(), name="submitted"),
 ]
